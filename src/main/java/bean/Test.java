e8c5039ec415e5b6b122ab24cf4f71710ca19828
yui_aramaki <yui_aramaki@LAPTOP-VAMNRGC7>
2026-04-28 11:24:36 +0900

    Add 得点beenを実装

---
+++ src/main/java/bean/Test.java
@@ -0,0 +1,62 @@
+package bean;
+
+import java.io.Serializable;
+
+public class Test implements Serializable {
+	
+	private Student student;
+	private String classNum;
+	private Subject subject;
+	private School school;
+	private int no;
+	private int point;
+	
+	 public Student getStudent() {
+	     return student;
+	 }
+
+	 public void setStudent(Student student) {
+	     this.student = student;
+	 }
+	  
+	 public String gerClassNum() {
+		 return classNum;
+	 }
+	 
+	 public void setClassNum(String classNum) {
+		 this.classNum = classNum;
+	 }
+	 
+	 public Subject getsubject() {
+		 return subject;
+	 }
+	 
+	 public void setSubject(Subject subject) {
+		 this.subject = subject;
+	 }
+	 
+	 public School getschool() {
+		 return school;
+	 }
+	 
+	 public void setSchool(School school) {
+		 this.school = school;
+	 }
+	 
+	 public int getNo() {
+		 return no;
+	 }
+	 
+	 public void setNO(int no) {
+		 this.no = no;
+	 }
+	 
+	 public int getPoint() {
+		 return point;
+	 }
+	 
+	 public void setPoint(int point) {
+		this.point = point; 
+	 }
+
+}
